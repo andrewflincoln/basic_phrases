be73dffe71031abfafb72c40d59a3145db9b1a8b
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-08-20 14:19:54 -0700

    first button working

--- app/src/main/java/com/andrewflincoln/basicphrases/MainActivity.java
+++ app/src/main/java/com/andrewflincoln/basicphrases/MainActivity.java
@@ -2,7 +2,9 @@ package com.andrewflincoln.basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void english(View view) {
+        MediaPlayer englishPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
+        englishPlayer.start();
+    }
+
 }
